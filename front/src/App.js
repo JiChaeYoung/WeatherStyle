daec75cb031d2562b0126da91e8ed7d17c7c7e43
SohnChaeWon <good6637@naver.com>
2023-10-10 17:54:12 +0900

    div 작업 mainPage

--- front/src/App.js
+++ front/src/App.js
@@ -1,49 +1,10 @@
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-`;
-
-const Qontainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  width: 100%;
-`;
-
-const Qiv = styled.div`
-  display: flex;
-  margin: 10px;
-  border: 1px solid black;
-  height: 100px;
-`;
-
-const Qiv2 = styled.div`
-  display: flex;
-  margin: 10px;
-  border: 1px solid black;
-  height: 100%;
-  margin-left: 20%;
-  margin-right: 20%;
-  padding: 10%;
-`;
-
-const Div = styled.div`
-  display: flex;
-  margin: 10px;
-  border: 1px solid black;
-  width: 250px;
-`;
+import Mainpage from './component/mainPage/MainPage';
 
 function App() {
   return (
-    <Container>
-      <Div>Div 1</Div>
-      <Qontainer>
-        <Qiv>Div 2</Qiv>
-        <Qiv2>Div 3</Qiv2>
-      </Qontainer>
-    </Container>
+    <div>
+      <Mainpage></Mainpage>
+    </div>
   );
 }
 
