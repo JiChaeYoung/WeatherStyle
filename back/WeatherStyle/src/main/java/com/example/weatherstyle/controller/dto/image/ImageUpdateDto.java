7edd07d9f3d7b34170df0dfcfea6e426f723e213
지채영 <wlcodud7@naver.com>
2023-11-03 15:52:33 +0900

    image수정을 위한 Dto

---
+++ back/WeatherStyle/src/main/java/com/example/weatherstyle/controller/dto/image/ImageUpdateDto.java
@@ -0,0 +1,12 @@
+package com.example.weatherstyle.controller.dto.image;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ImageUpdateDto {
+
+    private String content; //게시물에 글쓰기
+
+    private String weatherDescription;
+}
